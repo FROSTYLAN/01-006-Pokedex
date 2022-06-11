f4b8c66b70b5e2ce19144bfd83f83db1e8e8fdb8
FROSTYLAN <castillo089frosty@gmail.com>
2022-06-11 03:39:26 -0500

    fixed info

--- src/components/PokedexInfo.jsx
+++ src/components/PokedexInfo.jsx
@@ -24,61 +24,63 @@ const PokedexInfo = () => {
         <div className="rectangle small"></div>
       </div>
 
-      <div className="info-container">
-        <figure>
-          <img
-            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.order}.png`}
-            alt=""
-          />
-        </figure>
-        <h2># {pokemon.order}</h2>
-        <div className="name-pokemon">
-          <div className="line">
-            <h1>{pokemon.name}</h1>
+      <div className="main-info">
+        <div className="info-container">
+          <figure>
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.order}.png`}
+              alt=""
+            />
+          </figure>
+          <h2># {pokemon.order}</h2>
+          <div className="name-pokemon">
+            <div className="line">
+              <h1>{pokemon.name}</h1>
+            </div>
           </div>
-        </div>
-        <div className="stats-pokemon">
-          <div>
-            <p>Weight</p>
-            <b>{pokemon.weight}</b>
-          </div>
-          <div>
-            <p>Height</p>
-            <b>{pokemon.height}</b>
-          </div>
-        </div>
-
-        <section className="types-container">
-          <div className="type-container">
-            <h3>Type</h3>
-            <div className="type">
-              {pokemon.types?.map((types) => (
-                <div className={`abilities white-font ${types.type.name}`}>
-                  {types.type.name}
-                </div>
-              ))}
+          <div className="stats-pokemon">
+            <div>
+              <p>Weight</p>
+              <b>{pokemon.weight}</b>
+            </div>
+            <div>
+              <p>Height</p>
+              <b>{pokemon.height}</b>
             </div>
           </div>
 
-          <div>
-            <h3>Abilites</h3>
-            <div className="type">
-              {pokemon.abilities?.map((abilities) => (
-                <div className="abilities ">{abilities.ability.name}</div>
-              ))}
+          <section className="types-container">
+            <div className="type-container">
+              <h3>Type</h3>
+              <div className="type">
+                {pokemon.types?.map((types) => (
+                  <div className={`abilities white-font ${types.type.name}`}>
+                    {types.type.name}
+                  </div>
+                ))}
+              </div>
             </div>
-          </div>
-        </section>
-      </div>
 
-      <div className="movements-container">
-        <div className="title">
-          <h1>Movements</h1>
+            <div>
+              <h3>Abilites</h3>
+              <div className="type">
+                {pokemon.abilities?.map((abilities) => (
+                  <div className="abilities ">{abilities.ability.name}</div>
+                ))}
+              </div>
+            </div>
+          </section>
         </div>
-        <div className="moves">
-          {pokemon.moves?.map((moves) => (
-            <div className="move">{moves.move.name}</div>
-          ))}
+
+        <div className="movements-container">
+          <div className="title">
+            <h1>Movements</h1>
+          </div>
+          <div className="moves">
+            {pokemon.moves?.map((moves) => (
+              <div className="move">{moves.move.name}</div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
